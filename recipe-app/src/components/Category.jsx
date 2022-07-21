8e8a294c3f5eb2f8c28086bd4903c1350ed05de4
Dawid <dsienkiewicz689@gmail.com>
2022-07-21 23:00:05 +0200

    layout, buttons

---
+++ recipe-app/src/components/Category.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+import styled from "styled-components";
+// import { NavLink } from "react-router-dom";
+
+function Category() {
+  return (
+    // <List>
+    //   <NavLink to={"/cuisine/Italian"}>
+    //     <FaPizzaSlice />
+    //     <h4>Italian</h4>
+    //   </NavLink>
+    //   <NavLink to={"/cuisine/American"}>
+    //     <FaHamburger />
+    //     <h4>American</h4>
+    //   </NavLink>
+    //   <NavLink to={"/cuisine/Thai"}>
+    //     <GiNoodles />
+    //     <h4>Thai</h4>
+    //   </NavLink>
+    //   <NavLink to={"/cuisine/Japanese"}>
+    //     <GiChopsticks />
+    //     <h4>Japanese</h4>
+    //   </NavLink>
+    // </List>
+    <List>
+      <div>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </div>
+      <div>
+        <FaHamburger />
+        <h4>American</h4>
+      </div>
+      <div>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </div>
+      <div>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </div>
+    </List>
+  );
+}
+
+const List = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+
+export default Category;
